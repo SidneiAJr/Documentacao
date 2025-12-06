bd9c4a92ba9a1f51b7bc369f51337217c0d4e433
sidneiajr <sidalbertok@gmail.com>
2025-12-06 11:48:04 -0300

    teste

---
+++ a.bat
@@ -0,0 +1,50 @@
+@echo off
+echo ================================
+echo 🚀 BEM-VINDO AO AUTO-GIT v1.0
+echo ================================
+
+echo.
+echo 📂 Diretorio atual:
+cd
+echo.
+
+set /p confirm="Esta pasta e a que gostaria de clonar o repositorio? correto? (s/n): "
+
+if /I "%confirm%" NEQ "s" (
+    echo ❌ Operacao Cancelada.
+    pause
+    exit
+)
+
+echo.
+echo 🔄 Checando status...
+git status
+echo.
+
+echo 🔽 Executando GIT PULL...
+git pull
+echo.
+
+set /p msg="Digite a mensagem do commit: "
+
+if "%msg%"=="" (
+    echo ❌ Commit cancelado: mensagem vazia.
+    pause
+    exit
+)
+
+echo.
+echo ➕ Adicionando arquivos...
+git add .
+echo.
+
+echo 💬 Commitando...
+git commit -m "%msg%"
+echo.
+
+echo 🚀 Fazendo push para a branch atual...
+git push
+echo.
+
+echo ✅ Finalizado com sucesso!
+pause
